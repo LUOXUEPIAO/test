b9c84eb573d42af2bbc7ef22a3e54e6c556e38eb
asus <1715926634@qq.com>
2019-11-26 18:47:33 +0800

    垃圾分类数据处理1

--- p.py
+++ p.py
@@ -29,14 +29,13 @@ def get_files(file_path):
 
 train_imgs,train_lab = get_files("D:/garbage_classify/")
 
-def Img():
-    images = np.empty((14000,224,224,1),dtype="float32")
-    count=0
-    for i in train_imgs:
-        if count >= 14000:
-            break
+def Img(imgs):
+    lenth = len(imgs)
+    images = np.empty((lenth, 224,224,1),dtype="float32")
+    count = 0
+    for i in imgs:
         img = cv2.imread(i,0)
-        img = cv2.resize(img, (224, 224 ))
+        img = cv2.resize(img, (224, 224))
         #img = cv2.copyMakeBorder(img, 50, 50, 50, 50, cv2.BORDER_CONSTANT, value=(0, 0, 0))
         if img.shape == 4:
             img = img[:, :, :3]
@@ -45,12 +44,25 @@ def Img():
         img = np.array(img)
         img = np.expand_dims(img, axis=2)
         images[count, :, :, :] = img
-        count=count+1
+        count = count+1
     return images
-train_images = Img()
+train_images = Img(train_imgs)
 lables = np.array(train_lab)
 lables = get_dummies(lables)
 train_lables = lables.values
-print(train_images)
+test_imgs,test_lab = get_files("D:/test/")
 
+test_images = Img(test_imgs)
+tlables = np.array(test_lab)
+tlables = get_dummies(tlables)
+test_lables = tlables.values
 
+val_indices = np.random.choice(len(test_images),round(len(test_images)*0.5),replace=True)
+val_images = test_images[val_indices]
+val_lables = test_lables[val_indices]
+print(train_images.shape)
+print(train_lables.shape)
+print(test_images.shape)
+print(test_lables.shape)
+print(val_images.shape)
+print(val_lables.shape)
